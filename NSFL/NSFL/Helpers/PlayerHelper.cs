e810c9506c9556a2a557253defb524de1d2fb457
Noppadet <test@test.com>
2017-09-12 11:11:22 -0700

    Fix some player/announcement bugs

--- NSFL/NSFL/Helpers/PlayerHelper.cs
+++ NSFL/NSFL/Helpers/PlayerHelper.cs
@@ -12,15 +12,24 @@ namespace NSFL.Helpers
         {
             try
             {
-                if (playerFileLine.PlayerLine.Split('-').Length < 2 || playerFileLine.PlayerLine.Split(',').Length < 2)
+                if (playerFileLine.PlayerLine.Split('-').Length < 2 || playerFileLine.PlayerLine.Split(',').Length < 2 || !playerFileLine.PlayerLine.Trim().StartsWith("("))
                 {
-                    return new Player(); // Probably an inconsistenly formatted roster title
+                    return new Player(); // Probably an inconsistenly formatted roster title or announcement
                 }
 
                 var player = new Player();
                 player.PlayerSeasonDrafted = playerFileLine.PlayerLine.Split(')')[0].Replace("(", "").Trim();
-                player.PlayerFirstName = playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ')[0].Trim();
-                player.PlayerLastName = playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ')[1].Trim();
+
+                if (playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ').Length > 0)
+                {
+                    player.PlayerFirstName = playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ')[0].Trim();
+                    player.PlayerLastName = playerFileLine.PlayerLine.Split('-')[1].Trim().Split(' ')[1].Trim();
+                }
+                else // A last name is not required, prevent this player from being ommitted
+                {
+                    player.PlayerFirstName = playerFileLine.PlayerLine.Split('-')[1].Trim();
+                    player.PlayerLastName = "";
+                }
                 player.PlayerTPE = int.Parse(playerFileLine.PlayerLine.Split(',')[1].Split(':').Last().Trim());
                 player.PlayerPosition = playerFileLine.PlayerLine.Split('-').Last().Split(',')[0].Trim();
                 player.PlayerProfileURL = playerFileLine.PlayerLine.Split(',')[2].Trim();
